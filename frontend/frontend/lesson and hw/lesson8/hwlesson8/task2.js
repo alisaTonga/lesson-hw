5c7bcb3b74fcf55507bed56531574dc5fa03181a
Alisa <133078737+alisaTonga@users.noreply.github.com>
2024-04-11 17:34:22 +0200

    hw8 task 1 & 2

---
+++ frontend/frontend/lesson and hw/lesson8/hwlesson8/task2.js
@@ -0,0 +1,38 @@
+const a = 17;
+const b = 5;
+console.log(`a > b is ${a > b}`)
+console.log(`a <= b is ${a <= b}`)
+console.log(`a == b is ${a == b}`)
+console.log(`a != b is ${a != b}`)
+console.log();
+
+console.log(`'a' < 'b' is ${'a' < 'b'}`)
+console.log(`'ab' > 'a' is ${'ab' > 'a'}`)
+console.log();
+
+console.log(`'17' > 1 is ${'17' > 1}`)
+console.log();
+
+console.log(`17 === 1 is ${17 === 1}`)
+console.log(`17 === 17 is ${17 === 17}`)
+console.log(`17 === '17' is ${17 === '17'}`)
+console.log(`17 === true is ${17 === true}`)
+console.log(`'0' === '' is ${'0' === ''}`)
+console.log(`true === false is ${true === false}`)
+console.log(`true === true is ${true === true}`)
+console.log(`null === undefined is ${null === undefined}`)
+console.log(`false === 0 is ${false === 0}`)
+console.log();
+
+console.log(`17 == '17' is ${17 == '17'}`)
+console.log(`'0' == '' is ${'0' == ''}`)
+console.log(`0 == '' is ${0 == ''}`)
+console.log(`null == undefined is ${null == undefined}`)
+console.log(`false == 0 is ${false == 0}`)
+console.log();
+
+console.log(`undefined == null is ${undefined == null}`)
+console.log(`undefined == 0 is ${undefined == 0}`)
+console.log(`null == 0 is ${null == 0}`)
+console.log(`undefined < 0 is ${undefined < 0}`)
+console.log(`undefined > 0 is ${undefined > 0}`);
